4457102a8992fdbadcf9eafcabf30bb84d89b546
유정현 <105846655+dbdb1114@users.noreply.github.com>
2024-02-06 14:16:32 +0900

    [Silver II] Title: DFS와 BFS, Time: 416 ms, Memory: 25312 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,66 +1,77 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
 
 
 
 public class Main {
-static boolean visited[];
-    static ArrayList<Integer>[] A;
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt(); // 정점의 수
-        int M = scan.nextInt(); // 간선의 수
-        int Start = scan.nextInt(); // 시작점
-        A = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) {
-            A[i] = new ArrayList<Integer>();
+
+
+    static boolean[] visited;
+    static ArrayList<Integer>[] nodesConnection;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] firstCondition = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int N = firstCondition[0]; // 노드의 개수
+        int M = firstCondition[1]; // 에지의 개수
+        int V = firstCondition[2]; // 시작점
+
+
+        nodesConnection = new ArrayList[N+1];
+
+        for (int i = 0; i <= N; i++) {
+            nodesConnection[i] = new ArrayList<>();
         }
+
         for (int i = 0; i < M; i++) {
-            int S = scan.nextInt();
-            int E = scan.nextInt();
-            A[S].add(E);
-            A[E].add(S);
+            int[] connection = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            nodesConnection[connection[0]].add(connection[1]);
+            nodesConnection[connection[1]].add(connection[0]);
         }
-        // 방문할 수 있는 정점이 여러 개인 경우에는 번호가 작은 것을 먼저 방문 하기 위해 정렬
-        for (int i = 1; i <= N; i++) {
-            Collections.sort(A[i]);
+
+        for (int i = 0; i <= N; i++) {
+            Collections.sort(nodesConnection[i]);
         }
-        visited = new boolean[N + 1];  //방문 배열 초기화
-        DFS(Start);
+
+        visited = new boolean[N+1];
+        DFS(V);
         System.out.println();
-        visited = new boolean[N + 1];  //방문 배열 초기화
-        BFS(Start);
-        System.out.println();
+        visited = new boolean[N+1];
+        BFS(V);
+        bufferedReader.close();
 
     }
-
-    public static void DFS(int node) {  // DFS구현
-        System.out.print(node + " ");
-        visited[node] = true;
-        for (int i : A[node]) {
-            if (!visited[i]) {
+    public static void DFS(int start) {
+        System.out.print(start + " ");
+        visited[start] = true;
+        for (int i : nodesConnection[start]) {
+            if(!visited[i]){
                 DFS(i);
             }
         }
     }
 
-    private static void BFS(int node) {  // BFS구현
-        Queue<Integer> queue = new LinkedList<Integer>();
-        queue.add(node);
-        visited[node] = true;
+    public static void BFS(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        visited[start] = true;
+        queue.add(start);
 
-        while (!queue.isEmpty()) {
-            int now_node = queue.poll();
-            System.out.print(now_node + " ");
-            for (int i : A[now_node]) {
-                if (!visited[i]) {
-                    visited[i] = true;
-                    queue.add(i);
+        while(!queue.isEmpty()){
+            int now = queue.poll();
+            System.out.print(now +" ");
+            nodesConnection[now].stream().forEach(number -> {
+                if(!visited[number]){
+                    visited[number] = true;
+                    queue.add(number);
                 }
-            }
+            });
         }
     }
 }
