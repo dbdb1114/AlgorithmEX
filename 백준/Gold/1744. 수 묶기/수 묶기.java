38543dce6a26928dd9aba8ad0497b10936aeef7a
유정현 <105846655+dbdb1114@users.noreply.github.com>
2024-02-12 11:20:14 +0900

    [Gold IV] Title: 수 묶기, Time: 128 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -5,56 +5,48 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Stack;
 
 class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int number = Integer.parseInt(bufferedReader.readLine());
         int answer = 0;
 
-        ArrayList<Integer> numbers = new ArrayList<>();
+        PriorityQueue<Integer> plusNumber = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minusNumber = new PriorityQueue<>();
 
         for (int i = 0; i < number; i++) {
-            numbers.add(Integer.parseInt(bufferedReader.readLine()));
-        }
-
-        Map<Boolean, List<Integer>> listMap = numbers.stream().collect(partitioningBy(x -> x > 0));
-
-        List<Integer> plusNumber = listMap.get(true);
-        List<Integer> minusNumber = listMap.get(false);
-
-        plusNumber.sort(Comparator.reverseOrder());
-        minusNumber.sort(Comparator.naturalOrder());
-
-        for (int i = 0; i < plusNumber.size(); i+=2) {
-            try {
-                answer += multipleNumber(plusNumber.get(i) , plusNumber.get(i+1));
-            }catch (IndexOutOfBoundsException e ){
-                answer += plusNumber.get(i);
+            int readNumber =Integer.parseInt(bufferedReader.readLine());
+            if( readNumber == 1 ){
+                answer += readNumber;
+            } else if ( readNumber > 1 ) {
+                plusNumber.add(readNumber);
+            } else {
+                minusNumber.add(readNumber);
             }
         }
 
-        for (int i = 0; i < minusNumber.size(); i+=2) {
-            try {
-                answer += minusNumber.get(i) * minusNumber.get(i+1);
-            }catch (IndexOutOfBoundsException e ){
-                answer += minusNumber.get(i);
-            }
-        }
+        answer += bindNumber(plusNumber);
+        answer += bindNumber(minusNumber);
 
         System.out.println(answer);
     }
 
-    public static int multipleNumber (int num1, int num2){
-        boolean isBig = num1 * num2 > num1 + num2;
-        if(isBig){
-            return num1 * num2;
-        } else {
-            return num1 + num2;
+    public static int bindNumber(PriorityQueue<Integer> queue){
+        int answer = 0;
+        while (queue.size()>1){
+            answer += queue.poll() * queue.poll();
         }
+
+        if(queue.size() == 1){
+            answer += queue.poll();
+        }
+        return answer;
     }
 }
