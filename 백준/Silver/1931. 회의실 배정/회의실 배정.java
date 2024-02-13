41389fa27149c06869ad9ff4b1cf6b8fa46760d8
유정현 <105846655+dbdb1114@users.noreply.github.com>
2024-02-13 17:00:42 +0900

    [Silver I] Title: 회의실 배정, Time: 576 ms, Memory: 45312 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -2,86 +2,55 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 class Main {
 
-    private static int answer = 0;
+    static class Time implements Comparable<Time> {
+        int start;
+        int end;
+
+        public Time(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public int compareTo(Time time) {
+            if (this.end == time.end){
+                return this.start - time.start;
+            }
+            return this.end - time.end;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
         int conference = Integer.parseInt(bufferedReader.readLine());
-        Map<Integer,ArrayList<Integer>> conferencesTime = new HashMap<>();
-        Map<Integer, Integer> confferencesTime = new HashMap<>();
 
+        ArrayList<Time> tlist = new ArrayList<>();
         for (int i = 0; i < conference; i++) {
-            int[] readLine = Arrays.stream((bufferedReader.readLine().split(" "))).mapToInt(Integer::parseInt).toArray();
-
-            int start = readLine[0];
-            int end = readLine[1];
-
-            if(!conferencesTime.containsKey(start)){
-                conferencesTime.put(start,new ArrayList<>());
-            }
-            conferencesTime.get(start).add(end);
+            st = new StringTokenizer(bufferedReader.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            tlist.add(new Time(s,e));
         }
 
-        for (ArrayList<Integer> integers : conferencesTime.values()) {
-            integers.sort(Comparator.naturalOrder());
-        }
+        Collections.sort(tlist);
 
-        int endTime = 0;
+        int answer = 0;
+        int prev = 0;
 
-        List<Integer> keys = conferencesTime.keySet().stream().sorted().collect(Collectors.toList());
-
-        for (int key : keys) {
-            
-            Iterator<Integer> iterator = conferencesTime.get(key).iterator();
-            
-            int st = key;
-            int ed = iterator.next();
-
-            if(st < endTime){
-                continue;
-            }
-
-            while(isShort(conferencesTime, st,ed)){
+        for (Time time : tlist) {
+            if(time.start >= prev){
                 answer++;
-                endTime = ed;
-
-                if(st != ed){
-                    break;
-                }
-
-                if(iterator.hasNext()){
-                    ed = iterator.next();
-                } else {
-                    break;
-                }
+                prev=time.end;
             }
         }
-
         System.out.println(answer);
-    }
-
-    public static boolean isShort(Map<Integer,ArrayList<Integer>> times, int stTime, int edTime){
-        if(stTime == edTime) {
-            return true;
-        }
-
-        for (int i = stTime+1; i <= edTime; i++) {
-            if(times.containsKey(i) && times.get(i).get(0) < edTime){
-                return false;
-            }
-        }
-
-        return true;
     }
 }
