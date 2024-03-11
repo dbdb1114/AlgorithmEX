4799256bc2e844268b808e8e2adf5f878c80ee63
유정현 <105846655+dbdb1114@users.noreply.github.com>
2024-03-11 21:10:09 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 8.56 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,12 +1,9 @@
+import java.util.*;
+
 class Solution {
     public String solution(String[] seoul) {
         String answer = "";
-        for(int i = 0; i < seoul.length; i++){
-            if(seoul[i].equals("Kim")){
-                answer = "김서방은 " + i + "에 있다";
-                break;
-            }
-        }
+        answer = "김서방은 "+ Arrays.asList(seoul).indexOf("Kim") + "에 있다";
         return answer;
     }
 }
