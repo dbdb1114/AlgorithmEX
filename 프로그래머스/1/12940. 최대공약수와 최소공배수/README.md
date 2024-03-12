d3d50c69092b9f9af819a942f1b841900b7ad09c
유정현 <105846655+dbdb1114@users.noreply.github.com>
2024-03-12 17:12:45 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 3.73 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/README.md
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최대공약수와 최소공배수 - 12940 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12940) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12940#) 
 
 ### 성능 요약
 
-메모리: 71.7 MB, 시간: 2.15 ms
+메모리: 74.6 MB, 시간: 3.73 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 12일 17:08:16
+2024년 03월 12일 17:12:42
 
 ### 문제 설명
 
--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -10,24 +10,10 @@ class Solution {
         IntStream.rangeClosed(1,min).forEach(x -> {
             if(min % x == 0 && max % x == 0){
                 answer[0] = x;
+                answer[1] = n * m / answer[0];
             }
         });
         
-        int st1 = 1;
-        int st2 = 1;
-        
-        while(st1 <= max){
-            int minMul = min * st1;
-            int maxMul = max * st2;
-            if(minMul < maxMul){
-                st1++;
-            } else if (minMul > maxMul ){
-                st2++;
-            } else {
-                answer[1] = maxMul;
-                break;
-            }
-        }
 
         return answer;
     }
