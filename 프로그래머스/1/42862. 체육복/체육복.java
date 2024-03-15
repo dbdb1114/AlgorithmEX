d9628362063ce4581a3ccedef8fa1ce4537033dc
유정현 <105846655+dbdb1114@users.noreply.github.com>
2024-03-15 18:35:39 +0900

    [level 1] Title: 체육복, Time: 0.08 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -3,51 +3,38 @@ import java.util.*;
 
 class Solution {
     
-    public int solution(int n, int[] lost, int[] reserve) {
-    
-        int loPos = 0;
-        int rePos = 0;
-        
-        ArrayList<Integer> lostArr = new ArrayList<>();
-        ArrayList<Integer> resArr = new ArrayList<>();
-        
-        for(int i : lost){
-            lostArr.add(i);
+       public int solution(int n, int[] lost, int[] reserve) {
+        Set<Integer> lostSet = new HashSet<>();
+        Set<Integer> resSet = new HashSet<>();
+
+        for (int los : lost) {
+            lostSet.add(los);
         }
-        
-        for(int i : reserve){
-            resArr.add(i);
-        }
-        
-        Collections.sort(lostArr);
-        Collections.sort(resArr);
-       
-        ArrayList<Integer> removeArr = new ArrayList<Integer>();
-        for(int i : lostArr){
-            if(resArr.contains(i)){
-                removeArr.add(i);
+
+        for (int res : reserve) {
+            if(lostSet.contains(res)){
+                lostSet.remove(res);
+            } else {
+                resSet.add(res);
             }
         }
-        
-        lostArr.removeAll(removeArr);
-        resArr.removeAll(removeArr);
-        
-        int answer = n - lostArr.size();
-        
-        while(loPos < lostArr.size() && rePos < resArr.size()){
-            if(lostArr.get(loPos) + 1 == resArr.get(rePos) || 
-               lostArr.get(loPos) - 1 == resArr.get(rePos) ){
-                loPos++;
-                rePos++;
-                answer++;
-            } else if (lostArr.get(loPos) < resArr.get(rePos)){
-                loPos++;
-            } else if (lostArr.get(loPos) > resArr.get(rePos)){
-                rePos++;
+
+        Iterator<Integer> iterator = lostSet.iterator();
+        while (iterator.hasNext()) {
+            int stu = iterator.next();
+            if (resSet.contains(stu)) {
+                resSet.remove(stu);
+                iterator.remove();
+            } else if (resSet.contains(stu - 1)) {
+                resSet.remove(stu - 1);
+                iterator.remove();
+            } else if (resSet.contains(stu + 1)) {
+                resSet.remove(stu + 1);
+                iterator.remove();
             }
         }
-        
-        
-        return answer;
+
+        return n - lostSet.size();
     }
+
 }
