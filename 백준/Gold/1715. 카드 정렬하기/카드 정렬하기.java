31d0709bbdde6b0b16b3fc788379289250c4c502
유정현 <105846655+dbdb1114@users.noreply.github.com>
2024-04-04 15:18:21 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 364 ms, Memory: 25260 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -1,32 +1,27 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
-class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
-        int answer = 0;
-        Queue<Integer> card = new PriorityQueue<>();
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
 
         for (int i = 0; i < N; i++) {
-            card.offer(Integer.parseInt(bufferedReader.readLine()));
+            queue.add(Integer.parseInt(bf.readLine()));
         }
 
-        while (card.size() > 1){
-            int num1 = card.poll();
-            int num2 = card.poll();
+        int sum = 0;
+        while (queue.size() > 1){
+            Integer num1 = queue.poll();
+            Integer num2 = queue.poll();
 
-            int sum = num1 + num2;
-            card.offer(sum); // 정렬된 묶음 카드를 다른 카드와 다시 합치기 위한 과정 
-            answer+=sum; // 묶음 카드를 정렬하는데 확인한 횟수를 합하는 과정 
+            queue.add(num1+num2);
+            sum += num1 +num2;
         }
 
-        System.out.println(answer);
+
+        System.out.println(sum);
     }
-
 }
