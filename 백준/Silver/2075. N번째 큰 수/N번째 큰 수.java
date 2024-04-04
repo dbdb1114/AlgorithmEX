632e934f5467320655af010a8d6501e80fc508f6
유정현 <105846655+dbdb1114@users.noreply.github.com>
2024-04-04 17:59:21 +0900

    [Silver II] Title: N번째 큰 수, Time: 912 ms, Memory: 342316 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -8,7 +8,26 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bf.readLine());
         PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<String> strQueue = new PriorityQueue<>((a,b)->{
+            // 음수 처리
+            if((a.startsWith("-") && !b.startsWith("-"))||
+                a.length() > b.length()){
+                return 1;
+            } else if ((!a.startsWith("-") && b.startsWith("-"))||
+                        b.length() > a.length()){
+                return -1;
+            }
 
+            for (int i = 0; i < a.length(); i++) {
+                int res = Character.compare(a.charAt(i), b.charAt(i));
+                if(res != 0){
+                    return res;
+                }
+            }
+            return 0;
+        });
+
+        // 숫자 초기화
         for (int i = 0; i < N; i++) {
             String[] s = bf.readLine().split(" ");
             for (int j = 0; j < N; j++) {
@@ -16,6 +35,7 @@ public class Main {
             }
         }
 
+        // N번 직전까지 빼주기
         for (int i = 0; i < N-1; i++) {
             queue.poll();
         }
