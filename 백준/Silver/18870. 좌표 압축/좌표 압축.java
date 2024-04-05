1e9d33615035b51a67f7fca5b5234614f63107f7
유정현 <105846655+dbdb1114@users.noreply.github.com>
2024-04-05 17:02:09 +0900

    [Silver II] Title: 좌표 압축, Time: 2860 ms, Memory: 260720 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,40 @@
+import com.sun.source.tree.Tree;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+
+        StringTokenizer tk = new StringTokenizer(bf.readLine());
+
+        TreeMap<Long, Integer> treeMap = new TreeMap<>();
+        StringBuilder sb = new StringBuilder();
+        long[] nArr = new long[N];
+
+        for (int i = 0; i < N; i++) {
+            long num = Long.parseLong(tk.nextToken());
+            treeMap.put(num, 1);
+            nArr[i] = num;
+        }
+
+        int prev = 0;
+
+        for (Long aLong : treeMap.keySet()) {
+            Integer prev1 = treeMap.get(aLong);
+            treeMap.put(aLong, prev);
+            prev += prev1;
+        }
+
+        for (int i = 0; i < N; i++) {
+            sb.append(treeMap.get(nArr[i]) + " ");
+        }
+        sb.delete(sb.length()-1,sb.length());
+        System.out.println(sb);
+    }
+}
