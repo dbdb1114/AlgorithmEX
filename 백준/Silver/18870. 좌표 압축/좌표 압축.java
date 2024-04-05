2d5b1856199c454c814ac42c437b59cc93f0f5f7
유정현 <105846655+dbdb1114@users.noreply.github.com>
2024-04-05 17:28:03 +0900

    [Silver II] Title: 좌표 압축, Time: 2764 ms, Memory: 260544 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,8 +1,5 @@
-import com.sun.source.tree.Tree;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
 
