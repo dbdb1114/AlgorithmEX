4fe94484aec12df0dfbfd1dc8f70f03801ef275a
유정현 <105846655+dbdb1114@users.noreply.github.com>
2024-12-26 14:58:44 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 15100 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,34 +1,32 @@
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-
-        // 1. stack에 담는다.
-        // 2. peek가 '('이고, push 하는게 ')'라면 푸쉬하지 않고, 안에 있는 것을 팝한다.
-        Stack<Character> stack = new Stack<>();
-        String[] answer = new String[N];
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         for (int i = 0; i < N; i++) {
-            String s = bf.readLine();
-            findPS(stack, s);
-            answer[i] = stack.isEmpty() ? "YES" : "NO";
-            stack.clear();
-        }
-
-        for (String s : answer) {
-            System.out.println(s);
-        }
-    }
-    private static void findPS(Stack<Character> stack, String s){
-        for (int i = 0; i < s.length(); i++) {
-            if(!stack.isEmpty() && stack.peek() == '(' && s.charAt(i) == ')'){
-                stack.pop();
+            Stack<String> stack = new Stack<>();
+            String[] line = br.readLine().split("");
+            if(line.length == 0){
+                System.out.println("NO");
+                continue;
+            }
+            stack.push(line[0]);
+            for (int j = 1; j < line.length; j++) {
+                if(!stack.isEmpty()&&stack.peek().equals("(") && line[j].equals(")")){
+                    stack.pop();
+                } else {
+                    stack.push(line[j]);
+                }
+            }
+            if(stack.isEmpty()){
+                System.out.println("YES");
             } else {
-                stack.push(s.charAt(i));
+                System.out.println("NO");
             }
         }
     }
