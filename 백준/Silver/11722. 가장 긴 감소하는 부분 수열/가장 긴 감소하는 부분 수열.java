2406db106415d692d026c13d3f343ad7df76370e
유정현 <105846655+dbdb1114@users.noreply.github.com>
2025-01-01 13:27:39 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 120 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -1,31 +1,38 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        int[] dp = new int[N];
 
-    public static void main(String[] args) throws Exception{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-        int[] numbers = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int[] check = new int[N];
-        int answer = 1;
+        String[] line = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(line[i]);
+        }
 
-        check[N-1] = 1;
-        for (int i = N - 2; i >= 0; i--) {
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+        }
+
+        for (int i = N-2; i >= 0; i--) {
             for (int j = i+1; j < N; j++) {
-                if(numbers[j] < numbers[i]){
-                    check[i] = Math.max(check[i], check[j] + 1);
-                } else if (numbers[j] == numbers[i]){
-                    check[i] = Math.max(check[i], check[j]);;
-                } else {
-                    check[i] = Math.max(check[i],1);
+                if(A[i] > A[j]){
+                    dp[i] = Math.max(dp[i] , dp[j] + 1);
                 }
             }
-            answer = Math.max(answer, check[i]);
         }
-        
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            answer = Math.max(answer, dp[i]);
+        }
+
         System.out.println(answer);
     }
 }
