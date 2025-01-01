7b96fac2eb21d2d82fc0a07ceb12baebaff6e89d
유정현 <105846655+dbdb1114@users.noreply.github.com>
2025-01-01 14:46:37 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,43 +1,14 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
-class Main {
-    /**
-     * 문제
-     * 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
-     *
-     * 입력
-     * 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
-     *
-     * 출력
-     * 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
-     *
-     * 해결 방향
-     * 입력받은 수 까지 반복문을 출력한다
-     *  1. 입력받은 수 까지 지역변수를 증가시키고, 수만큼 별을 반복하여 찍는다.
-     *
-     * 테스트 케이스
-     * ------------------------------------------------------------------
-     * N = 1
-     * *
-     * ------------------------------------------------------------------
-     * N = 3
-     * *
-     * **
-     * ***
-     * ------------------------------------------------------------------
-     * */
-    public static void main(String[] args) throws IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-        StringBuilder answer = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
-            for (int j = 0; j < i; j++) {
-                answer.append("*");
-            }
-            answer.append("\n");
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= N; i++){
+            sb.append("*".repeat(i)).append("\n");
         }
-        System.out.println(answer);
+        System.out.println(sb);
     }
 }
