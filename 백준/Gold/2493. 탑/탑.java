97987028f9cf717d3419336db1028ea79001ab15
유정현 <105846655+dbdb1114@users.noreply.github.com>
2025-02-03 12:45:11 +0900

    [Gold V] Title: 탑, Time: 820 ms, Memory: 87940 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,52 +1,37 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bf.readLine());
-        int[] answer = new int[N];
-        int[] nArr = new int[N];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        Map<Integer,Integer> indexStore = new HashMap<>();
+		Stack<int[]> stack = new Stack<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < N; i++) {
-            int key = Integer.parseInt(st.nextToken());
-            nArr[i] = key;
-            indexStore.put(key, i);
-        }
+		for (int i = 1; i <= N; i++) { // 인덱스를 1부터 사용
+			int height = Integer.parseInt(st.nextToken());
 
-        Stack<Integer> stack = new Stack<>();
+			// 현재 탑보다 낮은 탑들은 수신할 수 없으므로 제거
+			while (!stack.isEmpty() && stack.peek()[0] < height) {
+				stack.pop();
+			}
 
-        for (int i = 0; i < nArr.length; i++) {
-            int tower = nArr[i];
-            while (!stack.isEmpty() && stack.peek() < tower){
-                Integer pop = stack.pop();
-                if(!stack.isEmpty()){
-                    answer[indexStore.get(pop)] = indexStore.get(stack.peek()) + 1;
-                } else {
-                    answer[indexStore.get(pop)] = 0;
-                }
-            }
-            stack.push(nArr[i]);
-        }
+			// 스택이 비어 있으면 신호를 받을 탑이 없음 (0)
+			if (stack.isEmpty()) {
+				sb.append("0 ");
+			} else {
+				// 가장 가까운 높은 탑의 인덱스를 출력
+				sb.append(stack.peek()[1]).append(" ");
+			}
 
-        while (!stack.isEmpty()) {
-            Integer pop = stack.pop();
-            if(!stack.isEmpty()){
-                answer[indexStore.get(pop)] = indexStore.get(stack.peek())+1;
-            } else {
-                answer[indexStore.get(pop)] = 0;
-            }
-        }
-        for (int i : answer) {
-            System.out.print(i + " ");
-        }
-    }
+			// 현재 탑을 스택에 저장 (탑의 높이, 인덱스)
+			stack.push(new int[]{height, i});
+		}
 
+		System.out.println(sb);
+	}
 }
