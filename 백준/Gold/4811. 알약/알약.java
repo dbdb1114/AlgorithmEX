64a206e098eddbfc67dea4c462a3e765e67ddcb7
유정현 <105846655+dbdb1114@users.noreply.github.com>
2025-02-24 11:49:39 +0900

    [Gold V] Title: 알약, Time: 108 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Gold/4811. 알약/알약.java
+++ 백준/Gold/4811. 알약/알약.java
@@ -3,17 +3,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+	private static long[][] memo = new long[31][31];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
+		dp(new int[] {30, 0}, 60, 0);
+
+		StringBuilder sb = new StringBuilder();
 		while(N != 0){
-			long result = dp(new int[] {N, 0}, 2 * N, 0);
-			System.out.println(result);
+			sb.append(memo[N][0]).append("\n");
 			N = Integer.parseInt(br.readLine());
 		}
+		System.out.println(sb);
 	}
 
-	private static long[][] memo = new long[50][50];
 	private static long dp(int[] pill, int totDay, int nowDay){
 		if(nowDay == totDay){
 			return 1;
