23ee8a7d3a374936a9dfe6acdd7d553f93af50ae
유정현 <105846655+dbdb1114@users.noreply.github.com>
2026-07-25 14:27:44 +0900

    [Passed] 일렬로 서있는 소 2 (121ms, 10MB)

--- trail2/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
+++ trail2/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -1,31 +1,41 @@
 import java.util.*;
 
 public class Main {
+
+    static int N;
+    static int[] cows;
+    static int answer = 0;
+
     public static void main(String[] args) {
         // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        int N = Integer.parseInt(sc.nextLine());
+        N = Integer.parseInt(sc.nextLine());
         StringTokenizer st = new StringTokenizer(sc.nextLine());
-
-        int[] cows = new int[N];
+        cows = new int[N];
 
         for(int i = 0; i < N; i++) {
             cows[i] = Integer.parseInt(st.nextToken());
         }
 
-        int answer = 0;
         for(int i = 0; i < N; i++) {
-            int ftCow = cows[i];
-            for(int j = i + 1; j < N; j++ ){
-                if(cows[j] < ftCow) continue;
-                int scCow = cows[j];
-                for(int k = j + 1; k < N; k++){
-                    if(cows[k] < scCow) continue;
-                    answer++;
-                }
-            }
+            recursive(i,1);
         }
 
         System.out.println(answer);
+    }
+
+    static void recursive(int n, int cnt) {
+        if(cnt >= 3) {
+            answer++;
+            return;
+        }
+
+        for(int i = n+1; i < N; i++){
+            if(cows[n] <= cows[i]) {
+                recursive(i, cnt + 1);
+            }
+        }
+
+        return;
     }
 }
